1369daa5a48f029d42e9116f2745abcc3792d37c
Tanveer <70311411+tanveer-ahmad74@users.noreply.github.com>
2023-11-29 22:05:57 +0500

    Update book.py

--- app/api/book.py
+++ app/api/book.py
@@ -1,5 +1,7 @@
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from app.authentication.auth import JWTBearer
 from app.crud import get, create, remove, update
 from app.schemas.book_schema import BookSchema
 from app.model import Book
@@ -9,7 +11,7 @@ from fastapi import APIRouter
 
 book_router = APIRouter()
 
-@book_router.post("/create", status_code=201)
+@book_router.post("/create", status_code=201, dependencies=[Depends(JWTBearer())])
 async def create_book(request: BookSchema, db: Session = Depends(get_db)):
     book_data = request.dict()
     _book = Book(title=book_data['title'], description=book_data['description'], user_id=book_data['user_id'])
@@ -17,7 +19,7 @@ async def create_book(request: BookSchema, db: Session = Depends(get_db)):
     return create_response
 
 
-@book_router.get("/get", status_code=200)
+@book_router.get("/get", status_code=200, dependencies=[Depends(JWTBearer())])
 async def get_all(db: Session = Depends(get_db)):
     books = get(db, Book)
     response = [
@@ -39,7 +41,7 @@ async def get_all(db: Session = Depends(get_db)):
     return list_response
 
 
-@book_router.put("/update/{book_id}", status_code=200)
+@book_router.put("/update/{book_id}", status_code=200, dependencies=[Depends(JWTBearer())])
 async def update_book(book_id: int, request: BookSchema, db: Session = Depends(get_db)):
     updated_data = request.dict()
     updated_book = update(db, Book, book_id, updated_data)
@@ -50,7 +52,7 @@ async def update_book(book_id: int, request: BookSchema, db: Session = Depends(g
         raise HTTPException(status_code=404, detail="Book not found")
 
 
-@book_router.delete("/delete/{book_id}", status_code=200)
+@book_router.delete("/delete/{book_id}", status_code=200, dependencies=[Depends(JWTBearer())])
 async def delete_book(book_id: int, db: Session = Depends(get_db)):
     deleted_book = remove(db, Book, book_id)
     if deleted_book:
