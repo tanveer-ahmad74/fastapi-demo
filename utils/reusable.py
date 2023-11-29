681b2f790f06a1961bdbc71a2ce343b4a27f35be
Tanveer <70311411+tanveer-ahmad74@users.noreply.github.com>
2023-11-29 22:07:41 +0500

    Update reusable.py

--- utils/reusable.py
+++ utils/reusable.py
@@ -1,9 +1,18 @@
 from app.model import User
 from sqlalchemy.orm import Session
 
+from utils.password_hasher import Hasher
+
 
 def check_email_exist(db: Session, email):
     query = db.query(User).filter(User.email == email).first()
     if query:
         return False
     return True
+
+def check_user(db: Session, email, password):
+    user = db.query(User).filter(User.email == email).first()
+    check_pass = Hasher.verify_password(password, user.password)
+    if check_pass:
+        return True
+    return False
